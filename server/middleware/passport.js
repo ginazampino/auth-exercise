500b6462871a66902a4b02551aa416fa22ba2ba7
Gina Zampino <zampino.gina@gmail.com>
2021-08-06 15:51:29 -0400

    Added .env to Passport.
    "ClientId" must be "ClientID".

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -1,7 +1,27 @@
 // Add Passport to the application:
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 module.exports = (app) => {
+    passport.use(new GoogleStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: 'http://localhost:' + process.env.PORT + '/google/callback',
+            passReqToCallback: true
+        },
+        (request, accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+        }
+    ));
 
+    // Serialize user:
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+
+    // Deserialize user:
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
 };
