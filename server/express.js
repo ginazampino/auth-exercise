2c7713f50c2acee9ec4b7585d1c3080196bc5b99
Gina Zampino <zampino.gina@gmail.com>
2021-08-06 14:35:20 -0400

    Added initial route to Express application.

--- server/express.js
+++ server/express.js
@@ -9,6 +9,9 @@ const app = express();
 // Use Express Session and refer to .env for session secret:
 app.use(session({ secret: process.env.SESSION_SECRET }));
 
+// Require middleware:
+require('./routes/router')(app);
+
 // Start the Express server on the specified port:
 app.listen(process.env.PORT, () => {
     console.log('Now listening on port ' + process.env.PORT + '.');
