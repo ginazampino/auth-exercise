15f93cbf296bb93eb83b696b9a507590a96a398c
Gina Zampino <zampino.gina@gmail.com>
2021-08-08 22:30:09 -0400

    Added additional middleware.

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,3 +1,21 @@
+// Add path from Node:
+const path = require('path');
+
+// Add Express for use by middleware:
+const express = require('express');
+
+// Add history support for Vue Router:
+const history = require('connect-history-api-fallback');
+
 module.exports.install = (app) => {
+    // Import external separate configurations:
     require('./passport')(app);
+    
+    // Map 404s to index.html for SPA support:
+    app.use(history());
+
+    // Enable Express to handle static files:
+    app.use(express.static(
+        path.resolve(__dirname, '../../dist')
+    ));
 }
