c4ad27d03a35681aebefe006ac8e358b9e2fc8aa
Gina Zampino <zampino.gina@gmail.com>
2021-08-12 21:05:25 -0400

    Added a named router link.

--- client/vue/App.vue
+++ client/vue/App.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <div id="nav">
-            <router-link to="/">Login</router-link> |
+            <router-link :to="{ name: 'Login' }">Login</router-link> |
             <router-link to="/profile">Profile</router-link>
         </div>
         <router-view/>
